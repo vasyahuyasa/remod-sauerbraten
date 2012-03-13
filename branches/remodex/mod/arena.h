8a4e65a8b73aab1f89d838b09d15cfdee23b24e5
evilbunny.x <evilbunny.x@ef6729cf-7dc3-3b3f-d63e-5a9ff8f948e7>
2012-03-13 19:45:46 +0000

    Added missed files

---
+++ branches/remodex/mod/arena.h
@@ -0,0 +1,16 @@
+/*
+* remod:    remodex.cpp
+* date:     2011
+* author:   degrave
+*
+* arena mode header
+*/
+
+#ifndef __ARENA_H__
+#define __ARENA_H__
+
+namespace remodex
+{
+    void arenamodeupdate();
+}
+#endif
