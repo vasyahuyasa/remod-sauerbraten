f920f56f2d93af7350e9eec649230554eb011507
evilbunny.x <vasyahuyasa@users.noreply.github.com>
2012-04-07 18:37:34 +0000

    Added comments

--- mod/cryptomod.cpp
+++ mod/cryptomod.cpp
@@ -163,11 +163,57 @@ namespace remod {
         result(hash);
     }
 
-    // Cube script bindings
+    /**
+    * Calculate the tiger192 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns tiger192 hash of string
+    * @example echo (tiger192 "test") // 8d1fd829fc83b37af1e5ba697ce8680d1d8bc430d76682f1
+    */
     COMMANDN(tiger192, cs_tiger192, "s");
+
+    /**
+    * Calculate the md5 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns md5 hash of string
+    * @example echo (md5 "test") // 098f6bcd4621d373cade4e832627b4f6
+    */
     COMMANDN(md5, cs_md5, "s");
+
+    /**
+    * Calculate the sha1 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns sha1 hash of string
+    * @example echo (sha1 "test") // a94a8fe5ccb19ba61c4c0873d391e987982fbbd3
+    */
     COMMANDN(sha1, cs_sha1, "s");
+
+    /**
+    * Calculate the sha256 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns sha256 hash of string
+    * @example echo (sha256 "test") // 9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08
+    */
     COMMANDN(sha256, cs_sha256, "s");
+
+    /**
+    * Calculate the sha384 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns sha384 hash of string
+    * @example echo (sha384 "test") // 768412320f7b0aa5812fce428dc4706b3cae50e02a64caa16a782249bfe8efc4b7ef1ccb126255d196047dfedf17a0a9
+    */
     COMMANDN(sha384, cs_sha384, "s");
+
+    /**
+    * Calculate the sha512 hash of a string
+    * @group crypto
+    * @arg1 string
+    * @return returns sha512 hash of string
+    * @example echo (sha512 "test") // ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff
+    */
     COMMANDN(sha512, cs_sha512, "s");
 }
