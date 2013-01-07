cfa1f77d0c3c33967fe43021ba616bca5a78d5c5
evilbunny.x <evilbunny.x@ef6729cf-7dc3-3b3f-d63e-5a9ff8f948e7>
2013-01-07 13:11:20 +0000

    Vanilla svn changes

--- trunk/src/engine/command.cpp
+++ trunk/src/engine/command.cpp
@@ -1156,6 +1156,7 @@ done:
 static bool compileword(vector<uint> &code, const char *&p, int wordtype, char *&word, int &wordlen)
 {
     skipcomments(p);
+retry:
     switch(*p)
     {
         case '\"': word = cutstring(p, wordlen); break;
@@ -1177,8 +1178,8 @@ static bool compileword(vector<uint> &code, const char *&p, int wordtype, char *
             return true;
         case '@':
             debugcode(debugline(p, "unexpected \"@\""));
-            p++;
-            return true;
+            do ++p; while(*p == '@');
+            goto retry;
         default: word = cutword(p, wordlen); break;
     }
     return word!=NULL;
