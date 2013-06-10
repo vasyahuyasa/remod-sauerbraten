863c01c1b24d2ccd4c3015b4c81f4612b7159cca
evilbunny.x <vasyahuyasa@users.noreply.github.com>
2013-06-10 14:05:24 +0000

    Rcon via telnet by Makefile option

--- src/mod/rconmod.h
+++ src/mod/rconmod.h
@@ -7,14 +7,16 @@ namespace remod
 namespace rcon
 {
 
-//Peer struct
+// Peer struct
 struct rconpeer
 {
+    int socket;
     struct sockaddr_in addr;
     bool logined;
     int connectmillis;
 };
 
+// interface
 void init(int port);
 void sendmsg(const char *msg, int len);
 void sendmsg(const char *msg);
