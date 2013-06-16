52a9d10790511ac436acf4f60e16252b14b3ba26
evilbunny.x <vasyahuyasa@users.noreply.github.com>
2013-06-16 21:58:55 +0000

    rcon tcp select timeout fix

--- src/mod/rconmod_tcp.cpp
+++ src/mod/rconmod_tcp.cpp
@@ -38,6 +38,10 @@ rconserver_tcp::rconserver_tcp(int port = 27070)
 {
     if(rconenable == 0) return;
 
+    // set select timeout for 1 usec
+    tv.tv_sec = 0;
+    tv.tv_usec = 1;
+
     // for setsockopt() SO_REUSEADDR
     yes = 1;
 
@@ -108,7 +112,7 @@ void rconserver_tcp::update()
     if(rconenable == 0) return;
 
     read_fds = master;
-    if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1)
+    if(select(fdmax+1, &read_fds, NULL, NULL, &tv) == -1)
     {
         conoutf("Rcon: select error");
         rconenable = 0;
