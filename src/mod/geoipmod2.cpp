fb1278873ce598b01715b167946ef698ddf19220
evilbunny.x@gmail.com <vasyahuyasa@users.noreply.github.com>
2014-08-08 15:09:38 +0000

    new geoip module

---
+++ src/mod/geoipmod2.cpp
@@ -0,0 +1,106 @@
+/*
+* remod:    geoipmod2.cpp
+* date:     2014
+* author:   degrave
+*
+* GEOIP new staff
+*/
+
+#include "GeoIP.h"
+#include "GeoIPCity.h"
+#include "geoipmod2.h"
+#include "remod.h"
+
+EXTENSION(GEOIP);
+
+namespace remod
+{
+namespace geoip
+{
+
+    static GeoIP *geoip = NULL;
+    static GeoIP *geocity = NULL;
+
+    void loadgeoip(const char *path, bool isgeocity)
+    {
+        GeoIP *gi = isgeocity ? geocity : geoip;
+        const char *fname = findfile(path, "r"); // full path
+        gi = GeoIP_open(fname, GEOIP_STANDARD | GEOIP_MEMORY_CACHE);
+        if(gi)
+        {
+            conoutf(CON_ERROR, "Geoip: %s loaded (db: \"%s\")", geocity ? "geocity" : "geoip", fname);
+        }
+        else
+        {
+            conoutf(CON_ERROR, "Geoip: can not load %s (db: \"%s\")", geocity ? "geocity" : "geoip", fname);
+        }
+    }
+
+    const char *getcountry(char *addr)
+    {
+        const char *country_name = NULL;
+        if(geoip)
+        {
+            GeoIPLookup gl;
+            country_name = GeoIP_country_name_by_addr_gl(geoip, addr, &gl);
+        }
+
+        return country_name;
+    }
+
+    const char *getcity(char *addr)
+    {
+        char *city = NULL;
+        if(geocity)
+        {
+            //GeoIPRecord *gir = GeoIP_record_by_ipnum(geocity, ipnum);
+            GeoIPRecord *gir = GeoIP_record_by_addr(geocity, addr);
+            if(gir != NULL)
+            {
+                char *city = gir->city != NULL ? newstring(gir->city) : NULL;
+                GeoIPRecord_delete(gir);
+            }
+        }
+
+        return city;
+    }
+
+    /**
+    * Load geoip database from specified path
+    * @group server
+    * @arg1 /path/to/geoip.db
+    */
+    ICOMMAND(geodb, "s", (const char *path),
+             {
+                 loadgeoip(path, false);
+             });
+
+    /**
+    * Load geoip city database from specified path
+    * @group server
+    * @arg1 /path/to/geoipcity.db
+    */
+    ICOMMAND(geocitydb, "s", (const char *path),
+             {
+                 loadgeoip(path, true);
+             });
+
+    /**
+    * Return country for specified ip
+    * @group server
+    * @arg1 ip
+    * @return country
+    */
+    COMMAND(getcountry, "s");
+
+    /**
+    * Return city for specified ip
+    * @group server
+    * @arg1 ip
+    * @return city
+    */
+    COMMAND(getcity, "s");
+
+}
+}
+
