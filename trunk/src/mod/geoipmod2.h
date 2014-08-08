d44f895469b4b200e24158a10c1d89d628cd3d09
evilbunny.x@gmail.com <evilbunny.x@gmail.com@ef6729cf-7dc3-3b3f-d63e-5a9ff8f948e7>
2014-08-08 15:09:38 +0000

    new geoip module

---
+++ trunk/src/mod/geoipmod2.h
@@ -0,0 +1,9 @@
+namespace remod
+{
+namespace geoip
+{
+    void loadgeoip(const char *path, bool geocity = false);
+    const char *getcountry(char *addr);
+    const char *getcity(char *addr);
+}
+}
