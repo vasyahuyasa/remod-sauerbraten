e7f8a292ec7ae89e0b2dbd8272f72708f20be3ff
vasyahuyasa <evilbunny.x@gmail.com>
2020-09-18 17:23:12 +0300

    don't save maxhealth on reconnect with new HB

--- src/fpsgame/fpsgame.h
+++ src/fpsgame/fpsgame.h
@@ -168,7 +168,7 @@ namespace server
     {
         uint ip;
         string name;
-        int maxhealth, frags, flags, deaths, teamkills, shotdamage, damage;
+        int frags, flags, deaths, teamkills, shotdamage, damage;
         int timeplayed;
         float effectiveness;
 
@@ -177,7 +177,6 @@ namespace server
 
         void save(gamestate &gs)
         {
-            maxhealth = gs.maxhealth;
             frags = gs.frags;
             flags = gs.flags;
             deaths = gs.deaths;
@@ -193,8 +192,6 @@ namespace server
 
         void restore(gamestate &gs)
         {
-            if(gs.health==gs.maxhealth) gs.health = maxhealth;
-            gs.maxhealth = maxhealth;
             gs.frags = frags;
             gs.flags = flags;
             gs.deaths = deaths;
